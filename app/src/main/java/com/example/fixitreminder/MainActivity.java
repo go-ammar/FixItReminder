5183c0b32d01036069700f284a223800b7cb6bb1
Ammar Ahsan <ammarahsan99@gmail.com>
2020-07-20 16:38:08 +0500

    components connected

--- app/src/main/java/com/example/fixitreminder/MainActivity.java
+++ app/src/main/java/com/example/fixitreminder/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.fixitreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText message;
+    Button mButton;
+    EditText mins;
+    EditText hours;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = (EditText) findViewById(R.id.EditTextmessage);
+        mButton = (Button) findViewById(R.id.saveButton);
+        mins = (EditText) findViewById(R.id.editTextMins);
+        hours = (EditText) findViewById(R.id.editTextHours);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO what happens when you click the save button? logic goes here.
+            }
+        });
+
     }
 }
