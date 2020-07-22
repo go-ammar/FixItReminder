5be7823d7365de37bfee3b70a85401f5a8937276
Ammar Ahsan <ammarahsan99@gmail.com>
2020-07-22 18:25:40 +0500

    onstop waghaira

--- app/src/main/java/com/example/fixitreminder/MainActivity.java
+++ app/src/main/java/com/example/fixitreminder/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.fixitreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     Button mButton;
     EditText mins;
     EditText hours;
+
+    boolean isScreenOn;
 
 
     @Override
@@ -29,8 +37,139 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO what happens when you click the save button? logic goes here.
+
             }
         });
 
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        isScreenOn = pm.isInteractive();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //TODO your background code
+                Log.d("humariApp", "onCreate " + isScreenOn);
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        isScreenOn = pm.isInteractive();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                isScreenOn = pm.isInteractive();
+                //Write your code here
+                Log.d("humariApp", "onStop with delay " + isScreenOn);
+            }
+        }, 5000);
+
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //TODO your background code
+                Log.d("humariApp", "onStop " + isScreenOn);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        isScreenOn = pm.isInteractive();
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //TODO your background code
+                Log.d("humariApp", "onPause " + isScreenOn);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        isScreenOn = pm.isInteractive();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                isScreenOn = pm.isInteractive();
+                //Write your code here
+                Log.d("humariApp", "onResume with delay " + isScreenOn);
+            }
+        }, 5000);
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //TODO your background code
+                Log.d("humariApp", "onResume " + isScreenOn);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        dontDestroyPls
+    }
+
+
 }
+
+
+/*
+
+AsyncTask.execute(new Runnable() {
+   @Override
+   public void run() {
+      //TODO your background code
+   }
+});
+
+    boolean checkIfScreenON(){
+
+    }
+
+    void Timer(int mins, int hours){
+            int curmins;
+            int curhours;
+            if (!checkIfScreenOn()){
+                curmins=0;
+                curhours=0;
+            }else {
+            timer start hogaya yahan pe hpor both mins and hours.
+            jab timer = mins, hours show popup.
+            }
+
+    }
+
+    void Timer(){
+
+    }
+
+
+
+    void main(){
+
+
+    for(execute()==true){
+        void timer();
+    }
+    }
+
+ */
